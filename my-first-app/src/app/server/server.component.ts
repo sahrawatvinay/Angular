8fbb3fa4d266fbe73c58ac1fae8f4b1374dabef3
vinay <vinaysahrawat183a@gmail.com>
2024-02-07 19:17:25 -0500

    clean

--- my-first-app/src/app/server/server.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-    selector: "app-server", //prefix with app, generally
-    templateUrl: "./server.component.html",
-})
-export class ServerComponent {
-}
