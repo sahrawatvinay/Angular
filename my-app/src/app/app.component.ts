ae02a4f0b5439b68279df5c5908310a1127a5b9a
vinay <vinaysahrawat183a@gmail.com>
2024-07-04 18:24:30 -0400

    create new components and add to app-component to view

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PostComponent } from './post/post.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, PostComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
