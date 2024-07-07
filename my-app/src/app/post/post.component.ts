6a722c23bfd676907e000be29fb28fa87ed0b6c0
vinay <vinaysahrawat183a@gmail.com>
2024-07-07 18:54:08 -0400

    2 way data binding

--- my-app/src/app/post/post.component.ts
+++ my-app/src/app/post/post.component.ts
@@ -1,10 +1,17 @@
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { FacebookProfileData } from '../models/facebook-profile-data';
-
+type Form = {
+  postTitle: string;
+  postDescription: string;
+  postImageUrl: string;
+  postUrl: string;
+  background: boolean;
+};
 @Component({
   selector: 'app-post',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './post.component.html',
   styleUrl: './post.component.css',
 })
@@ -15,6 +22,13 @@ export class PostComponent {
   postDataOnEMit: string = 'data from post (child) component when emitted';
   @Input() fromParentData!: FacebookProfileData;
   @Output() eventEmitterMsg = new EventEmitter<string>();
+  formData: Form = {
+    postTitle: '',
+    postDescription: '',
+    postImageUrl: '',
+    postUrl: '',
+    background: false,
+  };
   sendDataToParentByEmit() {
     console.log('CLiCKKCKSDDS');
     this.eventEmitterMsg.emit(this.postDataOnEMit);
