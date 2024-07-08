06d2a70420d10845c65867085fba8cf47be40411
vinay <vinaysahrawat183a@gmail.com>
2024-07-08 14:57:35 -0400

    angular directives

--- my-app/src/app/navbar/navbar.component.ts
+++ my-app/src/app/navbar/navbar.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
-
+  objArray: Array<any> = [
+    { id: 1, name: 'Ram', sub: 'Math' },
+    { id: 2, name: 'Sham', sub: 'Science' },
+    { id: 3, name: 'Karan', sub: 'SST' },
+    { id: 4, name: 'Bheem', sub: 'English' },
+    { id: 5, name: 'Drona', sub: 'Hindi' },
+  ];
+  isActive: boolean = false;
+  btnStepVal: string = '';
+  btnClick(value: string) {
+    this.btnStepVal = value;
+  }
+  constructor() {}
+  addItem(): void {
+    this.objArray.push({ id: 6, name: 'gg', sub: 'wp' });
+  }
+  removeItem(idx: number): void {
+    this.objArray.splice(idx, 1);
+  }
 }
