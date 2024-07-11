ef6e900caebfd928d04251c0f6866c01fcb817a5
vinay <vinaysahrawat183a@gmail.com>
2024-07-11 19:53:11 -0400

    using interfaces in angular

--- my-app/src/app/Services/user.service.ts
+++ my-app/src/app/Services/user.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  userList: Array<any> = [
+  userList: Array<User> = [
     { id: 1, name: 'Vinay' },
     { id: 2, name: 'Sam' },
     { id: 3, name: 'Dave' },
