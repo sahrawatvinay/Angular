292ff0caec519db758a98dd38bc3da010108653c
vinay <vinaysahrawat183a@gmail.com>
2024-07-29 13:17:08 +0530

    iterating arrays, *ngFor, *ngIf, binding data to properties and attributes

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { wishItem } from '../models/wishItem';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-app';
+  wishItems: wishItem[] = [
+    new wishItem('angular', false),
+    new wishItem('react', false),
+    new wishItem('node', true),
+    new wishItem('express', true),
+  ];
 }
---
+++ angular-app/src/models/wishItem.ts
@@ -0,0 +1,3 @@
+export class wishItem {
+  constructor(public wishText: string, public isCompleted: boolean = false) {}
+}
